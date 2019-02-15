00f980e1b8aedac0b502ebf08b7c8e8afd43422a
Etienne Danvoye <edanvoye@MacBook-Pro-de-Etienne.local>
2019-02-15 10:56:55 -0500

    demo provider stub

--- providers/demo.py
+++ providers/demo.py
@@ -3,8 +3,14 @@ from providers.base import ProviderPlugin
 
 class DemoBankPlugin(ProviderPlugin):
 
-    def update(self, get_user_data, store_user_data, add_account, add_transaction):
+    def update(self, get_user_data, store_user_data, add_account, add_transaction=None):
         pass
+        # ask user for demo bank username
+        # ask user for demo bank password
+        # check if credentials are ok
+        # fake login to website
+        # report 3 accounts found
+        # report transactions
 
     class meta:
         name = 'demo'
