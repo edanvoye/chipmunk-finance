5484621cb4621351ad5436092dfe8fffc8481349
Etienne Danvoye <edanvoye@MacBook-Pro-de-Etienne.local>
2019-02-22 22:55:04 -0500

    add functions to encrypt/decrypt strings

--- storage.py
+++ storage.py
@@ -3,6 +3,23 @@ import os
 import sqlite3
 import random
 
+def cipher_from_password(password, salt):
+    import base64
+    from cryptography.fernet import Fernet
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import hashes
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password.encode('utf-8')))
+    return Fernet(key)
+
 def sha1hash(s):
     import hashlib
     return hashlib.sha1(s.encode('utf-8')).hexdigest()
@@ -10,12 +27,25 @@ def sha1hash(s):
 class UserData():
     def __init__(self):
         self.conn = None
-        self.raw_pwd_hash = None
+        self.cipher = None
+
+    def encrypt(self, text):
+        if not self.cipher:
+            raise Exception('No Cipher')
+        return self.cipher.encrypt(text.encode('utf-8'))
+
+    def decrypt(self, text):
+        if not self.cipher:
+            raise Exception('No Cipher')
+        return self.cipher.decrypt(text).decode("utf-8")
 
     def exists(self, username):
         return os.path.exists(os.path.join('userdata', username + '.db'))
 
     def create(self, username, password):
+
+        self.conn = None
+        self.cipher = None
 
         if len(password)<6:
             raise Exception('Password too short')
@@ -29,8 +59,9 @@ class UserData():
             raise Exception('Database file already exists')
 
         # Generate salt and password hash
-        salt = sha1hash(str(random.random()))
-        password_hash = sha1hash('%s%s' % (salt,password))
+        salt1 = sha1hash(str(random.random()))
+        salt2 = sha1hash(str(random.random()))
+        password_hash = sha1hash('%s%s' % (salt1,password))
 
         # Create database
         conn = sqlite3.connect(db_file)
@@ -41,7 +72,8 @@ class UserData():
                                         id integer PRIMARY KEY,
                                         username text NOT NULL,
                                         password_hash text,
-                                        salt text
+                                        salt1 text,
+                                        salt2 text
                                     );
         '''
         c = conn.cursor()
@@ -49,30 +81,35 @@ class UserData():
 
         # Create one row in user table
         sql = ''' 
-            INSERT INTO users(username,password_hash,salt)
-              VALUES(?,?,?) '''
+            INSERT INTO users(username,password_hash,salt1,salt2)
+              VALUES(?,?,?,?) '''
         cur = conn.cursor()
-        ret = cur.execute(sql, (username,password_hash,salt))
+        ret = cur.execute(sql, (username,password_hash,salt1,salt2))
 
         conn.commit()
                 
         self.conn = conn
-        self.raw_pwd_hash = sha1hash(password)
+        self.cipher = cipher_from_password(password,bytes.fromhex(salt2))
 
     def open(self, username, password):
+
+        self.conn = None
+        self.cipher = None
 
         db_file = os.path.join('userdata', username + '.db')
         conn = sqlite3.connect(db_file)
 
         # Validate password with salt
         cur = conn.cursor()
-        cur.execute("SELECT salt,password_hash FROM users WHERE username=?", (username,))
+        cur.execute("SELECT salt1,salt2,password_hash FROM users WHERE username=?", (username,))
         row = cur.fetchone()
 
-        salt = row[0]
+        salt1 = row[0]
+        salt2 = row[1]
 
-        if not row[1] == sha1hash('%s%s' % (salt,password)):
+        if not row[2] == sha1hash('%s%s' % (salt1,password)):
             raise Exception('Wrong password')
 
         self.conn = conn
-        self.raw_pwd_hash = sha1hash(password)
+        self.cipher = cipher_from_password(password,bytes.fromhex(salt2))
+
