056626be11f1c03057bea55c8e3eda5b0cb63693
Etienne Danvoye <edanvoye@MacBook-Pro-de-Etienne.local>
2019-04-11 21:38:32 -0400

    add cache to currency converter

--- currency.py
+++ currency.py
@@ -1,24 +1,44 @@
 
 import requests
+import datetime
 
-# TODO cache
-# TODO fct to get all rates for base_currency
+class CurrencyCache():
+    def __init__(self):
+        self.date = datetime.date.today()
+        self.currency_cache = {}
+    def get(self, currency_from, currency_to):
+        if datetime.date.today() != self.date:
+            self.date = datetime.date.today()
+            self.currency_cache = {}
+        return self.currency_cache.get(currency_from + '_' + currency_to)
+    def add(self, currency_from, currency_to, value):
+        self.currency_cache[currency_from + '_' + currency_to] = value
+
+currency_cache = CurrencyCache()
 
 def currency_current_rate(currency_from, currency_to):
     if currency_from == currency_to:
         return 1.0
 
-    url = 'https://ratesapi.io/api/latest?base=%s&symbols=%s' % (currency_from, currency_to)
+    value = currency_cache.get(currency_from, currency_to)
+    if not value is None:
+        return value
+
+    url = 'https://api.ratesapi.io/api/latest?base=%s' % (currency_to)
     response = requests.get(url)
+
     if response.status_code==200:
         j = response.json()
-        if currency_to in j.get('rates'):
-            return j.get('rates')[currency_to]
+        for c in j.get('rates'):
+            currency_cache.add(c, currency_to, 1.0 / j.get('rates')[c])
+        if currency_from in j.get('rates'):
+            return 1.0 / j.get('rates')[currency_from]
     raise Exception('Error getting current exchange rate from %s to %s' % (currency_from, currency_to))
 
 if __name__ == "__main__":
 
     # currency API tests
 
-    for c1,c2 in [('USD','CAD'), ('CAD','USD'), ('CAD','EUR')]:
-        print('Current rate 1.00 %s = %.2f %s' % (c1,currency_current_rate(c1,c2),c2))
+    for _ in range(2):
+        for c1,base in [('USD','CAD'), ('CAD','USD'), ('CAD','EUR')]:
+            print('Current rate 1.00 %s = %.2f %s' % (c1,currency_current_rate(c1,base),base))
